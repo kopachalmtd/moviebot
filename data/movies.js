114bd585c0b8d5459a0a7b8a08beddc5a884d1bf
kopachalmtd <joykamaa433@gmail.com>
2025-11-14 15:03:05 +0300

    Create movies.js

---
+++ data/movies.js
@@ -0,0 +1,19 @@
+// a small JSON-like array used client & server-side
+const movies = [
+  {
+    id: "m1",
+    title: "Movie One",
+    price: 100,
+    poster: "/poster1.jpg",
+    tg_file_id: "FILE_ID_1"
+  },
+  {
+    id: "m2",
+    title: "Movie Two",
+    price: 150,
+    poster: "/poster2.jpg",
+    tg_file_id: "FILE_ID_2"
+  }
+];
+
+export default movies;
